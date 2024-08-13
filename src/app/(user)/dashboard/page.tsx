f36989415b01f88ae558e0a79de2d8b6e28a52ff
sajidmahamud835 <sajidmahamud835@gmail.com>
2024-08-13 14:04:15 +0600

    dashboard cards added

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -1,9 +1,19 @@
+import DashboardCards from '@/app/components/DashboardCards'
 import { NextPage } from 'next'
 
 interface Props { }
 
+const cardsData = [
+    { title: "BTC/USDT", text: "Slippage: 0.05% | Volume: $1.2B" },
+    { title: "ETH/USDT", text: "Slippage: 0.08% | Volume: $850M" },
+    { title: "BNB/USDT", text: "Slippage: 0.03% | Volume: $600M" },
+    { title: "ADA/USDT", text: "Slippage: 0.07% | Volume: $400M" },
+];
 const Dashboard: NextPage<Props> = ({ }) => {
-    return <div>Dasboard</div>
+    return <div>
+        <h1>Dashboard</h1>
+        <DashboardCards cards={cardsData} />
+    </div>
 }
 
 export default Dashboard
---
+++ src/app/components/DashboardCards.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { NextPage } from 'next'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+interface CardData {
+    title: string;
+    text: string;
+}
+
+interface Props {
+    cards: CardData[];
+}
+
+const DashboardCards: NextPage<Props> = ({ cards }) => {
+    return (
+        <div>
+            <Row xs={1} md={2} className="g-4">
+                {cards.map((card, idx) => (
+                    <Col key={idx}>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>{card.title}</Card.Title>
+                                <Card.Text>
+                                    {card.text}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    );
+}
+
+export default DashboardCards;
